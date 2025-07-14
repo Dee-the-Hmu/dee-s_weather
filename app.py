c0c7c84268f89435e38f17846b24f8f82c5d8b29
Dee-the-Hmu <aeinhmunaychi.ts@gmail.com>
2025-07-13 20:50:16 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, flash, redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime, timedelta
+from flask_session import Session
 
 from dotenv import load_dotenv
 
@@ -15,7 +16,7 @@ app = Flask(__name__)
 #configue session (may not need/use this)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+session(app)
 
 
 
