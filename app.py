9b58ea8d9913a10214d6203545c3fc5eecae99af
Dee-the-Hmu <aeinhmunaychi.ts@gmail.com>
2025-07-13 20:46:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 
-from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -19,8 +18,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-#connect to database
-db = SQL("sqlite:///any.db")
+
 
 #openweathermap.org
 load_dotenv()
